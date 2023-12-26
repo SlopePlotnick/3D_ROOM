4a874ce4e9b97532afa13c67b70a3abce572a77d
Rzy <815459626@qq.com>
2023-12-26 20:01:41 +0800

    完成环境配置

---
+++ cmake-build-debug/CMakeFiles/3D_ROOM.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "3D_ROOM"
+  "3D_ROOM.pdb"
+  "CMakeFiles/3D_ROOM.dir/lib/glad.c.o"
+  "CMakeFiles/3D_ROOM.dir/lib/glad.c.o.d"
+  "CMakeFiles/3D_ROOM.dir/main.cpp.o"
+  "CMakeFiles/3D_ROOM.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/3D_ROOM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OpenGL.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/plotnickslope/Desktop/学习资料/计算机图形学/作业/3D_ROOM/lib/glad.c" "CMakeFiles/OpenGL.dir/lib/glad.c.o" "gcc" "CMakeFiles/OpenGL.dir/lib/glad.c.o.d"
+  "/Users/plotnickslope/Desktop/学习资料/计算机图形学/作业/3D_ROOM/main.cpp" "CMakeFiles/OpenGL.dir/main.cpp.o" "gcc" "CMakeFiles/OpenGL.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
